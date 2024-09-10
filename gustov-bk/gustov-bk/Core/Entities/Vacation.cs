5fa4473c1122610fce36190d19b574da50c0ede6
agar777 <90040171+agar777@users.noreply.github.com>
2024-09-10 14:00:30 -0400

    Add entities and install Entity Framework packages

---
+++ gustov-bk/gustov-bk/Core/Entities/Vacation.cs
@@ -0,0 +1,8 @@
+public class Vacation
+{
+    public int Id {get;set;}
+    public int RequestId {get;set;}
+    public DateOnly StartDate {get;set;} 
+    public DateOnly EndDate {get;set;} 
+    public Request Request {get;set;}
+}
