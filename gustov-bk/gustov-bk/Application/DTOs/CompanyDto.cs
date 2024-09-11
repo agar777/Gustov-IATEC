a82a5c78eb8c5a29a1630f1bac63bd637181897e
agar777 <90040171+agar777@users.noreply.github.com>
2024-09-11 10:26:11 -0400

    Added DTOs files

---
+++ gustov-bk/gustov-bk/Application/DTOs/CompanyDto.cs
@@ -0,0 +1,6 @@
+public class CompanyDto
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public ICollection<User>? Users { get; set; }
+}
---
+++ gustov-bk/gustov-bk/Application/DTOs/RequestDto.cs
@@ -0,0 +1,10 @@
+public class RequestDto
+{
+    public int Id {get;set;}
+    public int UserId {get;set;}
+    public DateOnly RequestDate {get;set;}
+    public string? Status {get;set;}
+    public User? User {get;set;}
+    public ICollection<Vacation>? Vacations {get;set;}
+
+}
